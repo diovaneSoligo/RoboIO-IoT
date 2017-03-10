a15f1d32643357110033363417a4464f6da4fc6a
Diovane Soligo <diovane_soligo@hotmail.com>
2017-03-10 12:35:33 -0300

    robo funcional

---
+++ ControlaTomadasPhantom/src/DAO/BuscaDadosOnOff.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import modelo.OnOff;
+import postgresDB.Conexao;
+
+/**
+ *
+ * @author Evil
+ */
+public class BuscaDadosOnOff {
+    
+        public Object OnOff(int user) {
+    
+        ArrayList<OnOff> dados = new ArrayList<OnOff>();
+		try{
+			System.out.println("\nVai Buscar Dados");
+			
+			Connection c = Conexao.getConexao();//Faz conexão com banco
+			
+			PreparedStatement stmt =  c.prepareStatement("select *from dispositivos where usuario = "+user+" order by id_disp;");
+			
+			ResultSet rs = stmt.executeQuery();
+
+			while(rs.next()){
+				OnOff l = new OnOff();
+				
+				l.setUsuario(rs.getString("usuario"));
+                                l.setId_disp(rs.getString("id_disp"));
+                                l.setIp_disp(rs.getString("ip_disp"));
+                                l.setNome(rs.getString("nome"));
+                                l.setEstado(rs.getString("estado"));
+                               
+				dados.add(l);
+			}
+			System.out.println("tudo OK ... retornando dados\n");
+			c.close();
+		}catch(Exception e){
+			System.out.println("CATCH em Object OnOff em DAO BuscaDadosOnOff ...");
+			e.printStackTrace();
+		}
+		
+		return dados;
+}
+}
--- ControlaTomadasPhantom/src/controlatomadasphantom/ControlaTomadasPhantom.java
+++ ControlaTomadasPhantom/src/controlatomadasphantom/ControlaTomadasPhantom.java
@@ -5,38 +5,94 @@
  */
 package controlatomadasphantom;
 
+import DAO.BuscaDadosOnOff;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import modelo.OnOff;
 
 /**
  *
  * @author Evil
  */
 public class ControlaTomadasPhantom {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        String stringURL = "http://192.168.90.119:1000/?acao=002";
+/******************************************************************************/
+/******************************************************************************/
+    static class Busca implements Runnable {
+        int user = 1;//ID do usuario
+        
+        @Override
+        public void run() {
+            BuscaDadosOnOff X = new BuscaDadosOnOff();
+            
+            ArrayList<OnOff> Y = new ArrayList<OnOff>();
+            
+            Y = (ArrayList<OnOff>) X.OnOff(user);
+            
+            int tamanho = 0;
+            while(tamanho < Y.size()){
+                System.out.println("IP: "+Y.get(tamanho).getIp_disp()+" com: "+Y.get(tamanho).getEstado());
+                Executa exec = new Executa(Y.get(tamanho).getIp_disp(),Y.get(tamanho).getEstado());
+                new Thread(exec).start();
+                tamanho++;
+            }
+            System.out.println("...ENCERRA THREAD 1...");
+            
+        }
+    }
+/******************************************************************************/
+/******************************************************************************/    
+   static class Executa implements Runnable{
+       
+       String IP;
+       String estado;
+       
+       public Executa (String IP,String estado){
+       this.IP = IP;
+       this.estado = estado;
+       }
+       
+        @Override
+        public void run() {
+            String stringURL = "http://"+IP+":1000/?acao=00"+estado+"";
+            System.out.println("IP DE CONEXAO: "+stringURL);
         String resposta = "";
         try {
             URL url = new URL(stringURL);
             URLConnection connection = url.openConnection();
+            
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(
                     connection.getInputStream()));
+            
             String inputLine;
             StringBuffer sb = new StringBuffer();
+            
             while ((inputLine = in.readLine()) != null) sb.append(inputLine);
             resposta = sb.toString();
+            
             System.out.println("--> "+resposta);
+            
             in.close();
+            
+            System.out.println("...ENCERRA THREAD 2...");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+        }
+        
+   }
+/******************************************************************************/
+/******************************************************************************/   
+    public static void main(String[] args) {
+        
+        while(true){
+            Busca X = new Busca();
+            new Thread(X).start();
+            try { Thread.sleep (10000); } catch (InterruptedException ex) {}
+        }
     }
-    
+/******************************************************************************/    
 }
